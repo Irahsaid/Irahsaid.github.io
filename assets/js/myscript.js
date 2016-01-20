a756ab1da7b841123c3040d983ca8d0bb5f8448f
Irahsaid <irahnazirah@hotmail.com>
2016-01-20 09:25:11 +0800

    Deleted and tested some things

--- assets/js/myscript.js
+++
@@ -1,7 +0,0 @@
- $(document).ready(function () {
-     $("#click").click(function () {
-         $('html, body').animate({
-             scrollTop: $("#div1").offset().top
-         }, 2000);
-     });
- });
